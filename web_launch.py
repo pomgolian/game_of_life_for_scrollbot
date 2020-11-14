b6bd091d7744df716a41e67d8b4ca2e15997dd8c
chris <chris.pomgolian@gmail.com>
2020-11-14 15:16:18 +1100

    Added some todos

--- web_launch.py
+++ web_launch.py
@@ -2,9 +2,12 @@ from flask import Flask, render_template, url_for
 from threading import Thread
 import time
 from game_of_life import Life, display_life, display_scroll_text
-
-
 from werkzeug.utils import redirect
+
+# todo add dropdown list to select more apps
+# todo save highest iterations score to HDD for persistance
+# todo save initiation sequence for the highest iteration score to HDD and allow replay
+
 
 app = Flask(__name__)
 
@@ -25,21 +28,21 @@ def gol():
     global process_status
     global process_spawned
     process_spawned = True
-    iterations = 0
+    highest_iterations = 0
     while process_status == 'running': # process_run gets set to running from web page submit
         # the update_life method creates a new life sequence and returns repeating if sequence is repeating.
         # if we have a repeating sequence, quit current game, and start new game while process_run is true
         game = Life(17, 7) # create a new game with the size of the LED screen
         # print('new game created')
-        while game.update_life() == 'unique' and process_status == 'running': # keeps looping as long as no repeating patterns
-            # print_grid(game.life)
+        while game.update_life() == 'unique' and process_status == 'running':
+            # keeps looping as long as no repeating patterns
             display_life(game.life) # send the current game to display on the LED screen
             time.sleep(.5) # leave the display for a split second before refreshing
         # display the stats for each game before starting a new game.
         if game.update_life() == 'repeating': # end of game, patterns are now repeating
-            if game.iteration > iterations:
-                iterations = game.iteration
-            display_scroll_text('Current {} vs {}  '.format(game.iteration, iterations), 10)
+            if game.iteration > highest_iterations:
+                highest_iterations = game.iteration
+            display_scroll_text('Current {} vs {}  '.format(game.iteration, highest_iterations), 10)
         time.sleep(1)
     process_spawned = False
 
