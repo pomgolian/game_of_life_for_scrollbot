41a9c0e7d5b0dd87f2a1d07d9961b734b95dcf14
chris <chris.pomgolian@gmail.com>
2020-11-14 15:02:49 +1100

    Fixed the scrolled messages

--- game_of_life.py
+++ game_of_life.py
@@ -1,6 +1,8 @@
 import random
 import scrollphathd as sphd
 import time
+sphd.rotate(180)  # flip the screen for a scrollbot
+sphd.set_brightness(0.25)  # set a default brightness at 25%
 
 
 class Life:
@@ -68,9 +70,6 @@ class Life:
             return 'unique'
 
 
-# iterations = 0
-
-
 def display_life(current_game):
     sphd.clear() # blank the screen
     for key in current_game: # set our new pixels
@@ -79,34 +78,15 @@ def display_life(current_game):
             y = key//17
             # find X axis
             x = key - (y * 17)
-            sphd.set_pixel(x, y, 0.25)
-    sphd.show() # show the new screen
+            sphd.set_pixel(x, y, 1)  # set a brightness of 1 seeing as our default is already .25 and it is cumulative
+    sphd.show()  # show the new screen
 
 
 def display_scroll_text(text, display_time=5):
     update_time = 0.05  # scroll speed in seconds per pixel
+    sphd.clear()  # blank the screen ready to write some text
     sphd.write_string(text)
     for t in range(int(display_time / update_time)):
         sphd.show()
         sphd.scroll(1)
         time.sleep(update_time)
-
-
-# def run_life(horizontal, vertical):
-#     global iterations
-#     life = 'unique'
-#     game = Life(horizontal, vertical)
-#     while life == 'unique':
-#         if game.update_life():  # if this is true, we have a repeating sequence so we can quit current game
-#             print('Current game iteration was {}'.format(game.iteration))
-#             if game.iteration > iterations:
-#                 iterations = game.iteration
-#             for key in game.life:
-#                 print(game.life[key], end='')
-#                 if (key + 1) % horizontal == 0:
-#                     print('')
-#             print('------')
-#             life = 'repeating'
-#             print('Highest game iterations were {}'.format(iterations))
-#             print('------')
-#         time.sleep(0.2)
--- web_launch.py
+++ web_launch.py
@@ -37,11 +37,9 @@ def gol():
             time.sleep(.5) # leave the display for a split second before refreshing
         # display the stats for each game before starting a new game.
         if game.update_life() == 'repeating': # end of game, patterns are now repeating
-            display_scroll_text('Current game iteration was {}'.format(game.iteration), 5)
             if game.iteration > iterations:
                 iterations = game.iteration
-
-            display_scroll_text('Highest game iterations were {}'.format(iterations), 5)
+            display_scroll_text('Current {} vs {}  '.format(game.iteration, iterations), 10)
         time.sleep(1)
     process_spawned = False
 
